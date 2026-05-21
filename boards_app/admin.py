44f95ce91ed031a2afda4c02bf8c30d45a79f27e
Patricia Linne <patricia.linne@yahoo.de>
2026-05-21 09:18:54 +0200

    feat: adminpanales by board, task and comments

--- boards_app/admin.py
+++ boards_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.db.models import Count
+from .models import Board
 
-# Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'members_count')
+    search_fields = ('title', 'owner__fullname')
+    list_filter = ('owner',)
+    filter_horizontal = ('members',)
+    readonly_fields = ['owner']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(members_count=Count('members'))
+
+    def members_count(self, obj):
+        return obj.members_count
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.owner = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Board, BoardAdmin)
--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Task, Comments
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ['status', 'priority', 'board']
+    list_display = ['title', 'board', 'status',
+                    'priority', 'create_by', 'due_date']
+    search_fields = ['title', 'description',
+                     'create_by__email', 'create_by__fullname']
+    readonly_fields = ['create_by']
+    fieldsets = [
+        ('Key Information', {
+            'fields': ['board', 'title', 'description',
+                       'status', 'priority', 'due_date']
+        }),
+        ('Responsibilities', {
+            'classes': ['collapse'],
+            'fields': ['create_by', 'assignee', 'reviewer']
+        }),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.create_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_filter = ['author', 'created_at']
+    list_display = ['author', 'task', 'created_at']
+    readonly_fields = ['author']
+    search_fields = ['content', 'author__email', 'author__fullname']
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comments, CommentsAdmin)
